816fe2d59e47de6d3d98e728cedaf74836c76573
Jose Jhonata <contatojosejhonata@gmail.com>
2024-10-02 00:36:32 -0300

    Refatorando e Implementando os códigos para trabalhar com Multithread

--- script/script.js
+++ script/script.js
@@ -16,17 +16,6 @@ const graficoParaDolar = new Chart(graficoDolar, {
         },
 });
 
-//conectando com a api AwesomeAPI
-setInterval(() => conectaAPI(), 5000);
-async function conectaAPI() {
-    const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
-    const conectaTraduzido = await conecta.json();
-    let tempo = geraHorario();
-    let valor = conectaTraduzido.USDBRL.ask;
-    adicionarDados(graficoParaDolar, tempo, valor);
-    imprimeCotacao("dolar", valor);
-}
-
 //horarios
 function geraHorario() {
     let data = new Date();
@@ -44,3 +33,15 @@ function adicionarDados(grafico, legenda, dados) {
     });
     grafico.update();
 }
+
+//Trabalhando com Multithread
+
+let workerDolar = new Worker('./script/workers/workerDolar.js');
+workerDolar.postMessage('usd');
+
+workerDolar.addEventListener("message", event => {
+    let tempo = geraHorario();
+    let valor = event.data.ask;
+    imprimeCotacao("dolar", valor);
+    adicionarDados(graficoParaDolar, tempo, valor);
+})
